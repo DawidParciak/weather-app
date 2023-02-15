5aa9fea0fe25fbab6bcdb739ae8f96d0bb3d5440
Dawid <daw.parc@gmail.com>
2023-02-15 17:06:43 +0100

    Add API

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,11 +1,29 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
 
 const WeatherBox = props => {
+
+  const handleCityChange = useCallback(city => {
+    console.log('City: ', city);
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=37066b1cca04b9508cf4ea94cac08457&units=metric`)
+    .then(res => res.json())
+    .then(data => {
+      const weatherData = {
+        city: data.name,
+        temp: data.main.temp,
+        icon: data.weather[0].icon,
+        description: data.weather[0].main
+      };
+      console.log(weatherData);
+    });
+  });
+
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange}/>
       <WeatherSummary />
       <Loader />
     </section>
